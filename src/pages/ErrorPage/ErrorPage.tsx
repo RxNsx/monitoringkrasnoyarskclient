c0673376a58d32e4a38dc2bdeaf1171526149c43
Vadim Sbitnev <alpendev@mail.ru>
2025-03-18 04:44:14 +0300

    Добавляет страницу ошибок

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -2,12 +2,12 @@ import {Link} from "react-router-dom";
 
 export default function ErrorPage() {
     return (
-        <>
+        <div className="m-auto p-3">
             <h1>Такой страницы не существует</h1>
             <p>Пожалуйста сообщите об ошибке по следующему электронному адресу alpendev@mail.ru</p>
             <Link to="/">
                 Вернуться на главную страницу
             </Link>
-        </>
+        </div>
     )
 }
