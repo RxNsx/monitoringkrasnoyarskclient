b1a0890378a8cd3fb29d9d2dc021f96fae67d10f
Vadim Sbitnev <alpendev@mail.ru>
2025-05-06 08:05:48 +0300

    Добавляет возможность отображение событий по конкретной улице

--- src/features/GetGeoLocationData/getGeoLocationData.ts
+++ src/features/GetGeoLocationData/getGeoLocationData.ts
@@ -22,3 +22,25 @@ export async function getGeoLocationDataByDistrictId(districtId : string | undef
             return [...data];
         })
 }
+
+export async function getGeoLocationDataByStreetId(streetId : string | undefined) : Promise<GeoLocationData[]> {
+    const url = `http://127.0.0.1:5000/api/Geocode/GetStreetGeoCoordinates?streetId=${streetId}`;
+
+    return await fetch(url, {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        method: "GET"
+    })
+        .then(response => {
+            if(!response.ok) {
+                throw new Error(`Ошибка получения геоданных по улице ${streetId}`);
+            }
+
+            return response.json();
+        })
+        .then(data => {
+            return [...data];
+        })
+}
